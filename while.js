d2c6871aa8bdfc72a99da15772bed78e31dead46
Aleajndro <hgaviria@cesde.net>
2024-09-13 01:56:41 +0200

    EJEMPLO WHILE CON INDEX.HTML

---
+++ while.js
@@ -0,0 +1,8 @@
+const numero = 5;
+let numero1 = parseInt(prompt('INGRESE UN DATO NUMERICO'));
+while(numero1<1000){
+    numero1 = numero1*numero
+    document.write(numero1+"<br>")
+}
+    document.write(numero1+"<br>")
+    
